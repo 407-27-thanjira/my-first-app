fb1880d2d0fc79adb38a9d4ff8b44bcaaf831c1a
407-27-thanjira <54639@yupparaj.ac.th>
2026-08-27 13:14:21 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -43,23 +43,9 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-    # ตรวจข้อ 3
-    if ans3.strip().lower() == "pizza":
-        st.success("✅ ข้อ 3: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
-
-   # ตรวจข้อ 4
-    if ans4.strip().lower() == "horse":
-        st.success("✅ ข้อ 4: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
-
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
     
-    if score == 4:
+    if score == 2:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -91,19 +77,10 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.get("ans2_val", ""),
 )
-ans3 = st.text_input(
-    "ข้อ 3: I love to eat `p i _ _ a`. 🍕",
-  value=st.session_state.get("ans3_val", ""),
-)
-ans4 = st.text_input(
-    "ข้อ 4: I love to eat `h o _  s _`. 🐎",
-   value=st.session_state.get("ans4_val", ""),
-)
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
+
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
